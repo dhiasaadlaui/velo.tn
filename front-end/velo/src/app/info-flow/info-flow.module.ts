7f4df6996f4ef8cde6f3399b33a22188494c7310
Mohamed Dhia Saadlaoui <mdsaadlaoui@vermeg.com>
2020-07-21 19:56:00 +0100

    stories

--- front-end/velo/src/app/info-flow/info-flow.module.ts
+++ front-end/velo/src/app/info-flow/info-flow.module.ts
@@ -6,16 +6,18 @@ import { InfoFlowComponent } from './info-flow.component';
 import { ChartsModule } from 'ng2-charts';
 import { StoriesComponent } from './components/stories/stories.component';
 import { DataFlowComponent } from './components/data-flow/data-flow.component';
-
-
+import { MatCardModule } from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 @NgModule({
   declarations: [InfoFlowComponent, StoriesComponent, DataFlowComponent],
   imports: [
     CommonModule,
     InfoFlowRoutingModule,
-    ChartsModule
+    ChartsModule,
+    MatCardModule,
+    MatButtonModule
   ],
-  exports:[
+  exports: [
     InfoFlowComponent,
     DataFlowComponent
   ]
