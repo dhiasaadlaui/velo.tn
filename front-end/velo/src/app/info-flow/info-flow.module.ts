48ac7a20a485bd324aadbaaeb0a25b9b2ace6e85
Ali HABIB <ahabib@vermeg.com>
2020-07-21 16:22:56 +0200

    update info-flow

--- front-end/velo/src/app/info-flow/info-flow.module.ts
+++ front-end/velo/src/app/info-flow/info-flow.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { InfoFlowRoutingModule } from './info-flow-routing.module';
 import { InfoFlowComponent } from './info-flow.component';
+import { ChartsModule } from 'ng2-charts';
+import { StoriesComponent } from './components/stories/stories.component';
+import { DataFlowComponent } from './components/data-flow/data-flow.component';
 
 
 @NgModule({
-  declarations: [InfoFlowComponent],
+  declarations: [InfoFlowComponent, StoriesComponent, DataFlowComponent],
   imports: [
     CommonModule,
-    InfoFlowRoutingModule
+    InfoFlowRoutingModule,
+    ChartsModule
   ]
 })
 export class InfoFlowModule { }
