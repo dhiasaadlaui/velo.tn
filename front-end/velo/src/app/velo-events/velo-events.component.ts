352dd3b5ee6d086da9dc3523fc061e008585991f
Jubran-Saoudi <saoudi.jebrane@gmail.com>
2020-07-26 03:16:37 +0100

    scheduler features implemented velo-events

--- front-end/velo/src/app/velo-events/velo-events.component.ts
+++ front-end/velo/src/app/velo-events/velo-events.component.ts
@@ -21,7 +21,7 @@ import { UserService } from '../core/services/user.service';
   encapsulation: ViewEncapsulation.None
 
 })
-export class VeloEventsComponent implements OnInit,OnDestroy {
+export class VeloEventsComponent implements OnInit, OnDestroy {
   @ViewChild('scheduleObj', { static: false }) scheduleObj: ScheduleComponent;
   // DONT TOUCH !!
   @ViewChild('grid', { static: false })
@@ -30,11 +30,11 @@ export class VeloEventsComponent implements OnInit,OnDestroy {
   // ** PONTERS ** //
   selectedrecords: object[]
   operationTobeExecuted: string = '';
-  eventToShowDetails:EventEntity;
+  eventToShowDetails: EventEntity;
   //
 
   // Subscriptions //
-  eventSubscription$:any;
+  eventSubscription$: any;
 
   /**
   * DATA.
@@ -77,14 +77,14 @@ export class VeloEventsComponent implements OnInit,OnDestroy {
   public currentMinute: number = this.today.getMinutes();
   public currentSecond: number = this.today.getSeconds();
   public date: Date = new Date(new Date().setDate(14));
-  public minDate: Date = new Date(this.currentYear,this.currentMonth,7,0,0,0);
-  public maxDate: Date = new Date(this.currentYear,this.currentMonth,27,this.currentHour,this.currentMinute,this.currentSecond);
+  public minDate: Date = new Date(this.currentYear, this.currentMonth, 7, 0, 0, 0);
+  public maxDate: Date = new Date(this.currentYear, this.currentMonth, 27, this.currentHour, this.currentMinute, this.currentSecond);
   /**
  * Flags.
  */
   showCalendar: boolean = true;
   showMoreEvents: boolean = false;
-  ShowEventDetails:boolean =false;
+  ShowEventDetails: boolean = false;
 
   /**
  * Creates an instance of VeloEventsComponent.
@@ -98,7 +98,7 @@ export class VeloEventsComponent implements OnInit,OnDestroy {
       });
     });
 
-     this.setView = 'Month';
+    this.setView = 'Month';
     this.eventSettings = {
       dataSource: this.data,
       fields: {
@@ -135,54 +135,53 @@ export class VeloEventsComponent implements OnInit,OnDestroy {
   /**
    * Search for events that match Criteria and added to the calendar the result .
    */
-  find() {
-    this.events.forEach(element => {
-      this.scheduleObj.addEvent({
-        Id:  element.id,
-        EventName: element.event_name + 'd',
-        StartTime: new Date(element.start_date),
-        EndTime: new Date(element.end_date),
+  showMyEvents() {
+    console.log(this.scheduleObj);
+    let localArrayObject: Object[] = [];
+    this.myEvents.forEach(e => {
+      localArrayObject.push({
+        Id: e.id,
+        EventName: e.event_name,
+        StartTime: new Date(e.start_date),
+        EndTime: new Date(e.end_date),
         IsAllDay: false,
         IsReadonly: false,
         fddf: 'df',
-        //RecurrenceRule: 'FREQ=WEEKLY;BYDAY=MO,TU,WE,TH,FR;INTERVAL=1',
-        CategoryColor: '#357cd2'
-      })
+      });
     });
-    /*this.event = this.events.find(element => element.id == 50);
-    this.scheduleObj.addEvent({
-      Id: this.event.id,
-      EventName: this.event.event_name + 'd',
-      StartTime: new Date(2020, 6, 27, 10, 0),
-      EndTime: new Date(2020, 6, 27, 10, 0),
-      IsAllDay: false,
-      IsReadonly: false,
-      fddf: 'df',
-      RecurrenceRule: 'FREQ=WEEKLY;BYDAY=MO,TU,WE,TH,FR;INTERVAL=1',
-      CategoryColor: '#357cd2'
-    })*/
+    this.scheduleObj.eventSettings.dataSource = localArrayObject;
   }
 
-  testRest(){
-    this.scheduleObj.resetEventTemplates()
-  }
 
   ngOnInit() {
-     this.eventSubscription$ = this.eventServ.todos.subscribe(updatedTodos => {
+    this.eventSubscription$ = this.eventServ.todos.subscribe(updatedTodos => {
       this.events = [];
       this.events = updatedTodos;
       this.myEvents = [];
+      let localArrayObject: Object[] = [];
       updatedTodos.forEach(e => {
-        if(e.creator_user_id === this.userService.getCurrentUser().username){
+        if (e.creator_user_id === this.userService.getCurrentUser().username) {
           this.myEvents.push(e);
         }
-      })
+            localArrayObject.push({
+            Id: e.id,
+            EventName: e.event_name,
+            StartTime: new Date(e.start_date),
+            EndTime: new Date(e.end_date),
+            IsAllDay: false,
+            IsReadonly: false,
+            fddf: 'df',
+            //RecurrenceRule: 'FREQ=WEEKLY;BYDAY=MO,TU,WE,TH,FR;INTERVAL=1',
+            CategoryColor: '#357cd2'
+          });
+         this.scheduleObj.eventSettings.dataSource = localArrayObject;
+      });
     });
     this.eventServ.loadAll();
     this.editSettings = { allowEditing: true, allowAdding: true, allowDeleting: true, mode: 'Dialog' };
     this.toolbar = ['Add', 'Edit', 'Delete',
       { text: 'Archive', tooltipText: 'Archive', prefixIcon: 'e-save', id: 'Archive' },
-      { text: 'Refresh', tooltipText: 'Refresh', prefixIcon: 'e-reload	', id: 'Refresh' }];
+      { text: 'Async', tooltipText: 'Async', prefixIcon: 'e-reload', id: 'Refresh' }];
     this.pageSettings = { pageCount: 5 };
     let i = 0;
 
@@ -197,7 +196,7 @@ export class VeloEventsComponent implements OnInit,OnDestroy {
 
   }// -> ** ON INIT END
   ngOnDestroy(): void {
-    this.eventSubscription$.unsubscribe(); 
+    this.eventSubscription$.unsubscribe();
   }
 
 
@@ -224,7 +223,7 @@ export class VeloEventsComponent implements OnInit,OnDestroy {
       let selectedEvent: any = this.scheduleObj.activeEventData.event;
       this.eventServ.remove(selectedEvent.Id)
     }
-    this.scheduleObj.getSelectedElements().forEach((element) => {      
+    this.scheduleObj.getSelectedElements().forEach((element) => {
       console.log(element)
     });
   }
@@ -245,7 +244,7 @@ export class VeloEventsComponent implements OnInit,OnDestroy {
 
   public onPopupOpen(args: PopupOpenEventArgs): void {
     console.log(this.scheduleObj);
-    
+
     this.scheduleObj.getEvents().forEach((element: any) => console.log(element.Id))
     let selectedEvent: any = this.scheduleObj.activeEventData.event;
     console.log(selectedEvent)
@@ -261,7 +260,8 @@ export class VeloEventsComponent implements OnInit,OnDestroy {
 
   public onEventCheck(args: any): boolean {
     let eventObj: any = args.data instanceof Array ? args.data[0] : args.data;
-    return (eventObj.StartTime < new Date());
+    //return (eventObj.StartTime < new Date());
+    return true;
   }
 
 
@@ -328,18 +328,17 @@ export class VeloEventsComponent implements OnInit,OnDestroy {
         let data: any;
         data = this.orderForm.value;
         let eventLocal: EventEntity = this.eventServ.buildEvent(data);
-        if(new Date(eventLocal.start_date) > new Date(eventLocal.end_date)){
+        if (new Date(eventLocal.start_date) > new Date(eventLocal.end_date)) {
           alert("End Date Cant be greater then start Date please change inputs")
           return;
         }
         if (this.operationTobeExecuted != '') {
           console.log('OPERATION START ......')
           if (this.operationTobeExecuted === 'update') {
-            console.log('UPDATE START ......')           
+            console.log('UPDATE START ......')
             this.eventServ.update(eventLocal);
-            this.eventServ.loadAll()
           } else if (this.operationTobeExecuted === 'create') {
-            if(eventLocal.is_archived == null){
+            if (eventLocal.is_archived == null) {
               eventLocal.is_archived = false;
             }
             console.log('CREATE START ......')
@@ -390,15 +389,15 @@ export class VeloEventsComponent implements OnInit,OnDestroy {
       if (this.selectedEvent == null) {
         alert("please select Event")
       } else {
-        if(this.selectedEvent.is_archived == true){
+        if (this.selectedEvent.is_archived == true) {
           alert("Event already archived")
-        }else {
+        } else {
           this.eventServ.archiver(this.selectedEvent);
           alert('Event archived successfully')
           this.eventServ.loadAll()
         }
       }
-    }else if (args.item.id === 'Refresh') {
+    } else if (args.item.id === 'Refresh') {
       this.eventServ.loadAll()
     }
 
