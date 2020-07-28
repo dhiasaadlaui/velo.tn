4ffe8d4e767f3a60e5c94a4a1645e5bd8363e067
unknown <dhiasaadlaui@gmail.com>
2020-07-28 02:34:16 +0100

    fixed compilation error front end

--- front-end/velo/src/app/app-routing.module.ts
+++ front-end/velo/src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { LoginadminComponent } from './loginadmin/loginadmin.component';
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
