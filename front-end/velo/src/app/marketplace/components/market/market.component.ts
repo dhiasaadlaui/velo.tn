8e247b64c579366ed7c85c220a28bd9033cbf3b2
unknown <dhiasaadlaui@gmail.com>
2020-07-28 07:16:21 +0100

    market activities

--- front-end/velo/src/app/marketplace/components/market/market.component.ts
+++ front-end/velo/src/app/marketplace/components/market/market.component.ts
@@ -1,3 +1,5 @@
+import { MarketplaceService } from './../../../core/services/marketplace.service';
+import { Product, Auction } from './../../../core/models/marketplace';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MarketComponent implements OnInit {
 
-  constructor() { }
+  products: Product[] = [];
+  availableProducts: Product[] = [];
+  auctions: Auction[] = [];
+
+
+  constructor(
+    private _marketplaceService: MarketplaceService
+  ) { }
 
   ngOnInit() {
+    this.initializeProducts();
+    this.initializeAvailableProducts();
+    this.initializeAuctions();
   }
+
+
+  initializeProducts() {
+    this._marketplaceService.getProducts()
+      .subscribe(
+        products => { this.products = products; },
+        err => { }
+      )
+  }
+  initializeAvailableProducts() {
+    this._marketplaceService.getAvailableProducts()
+      .subscribe(
+        availableProducts => { this.availableProducts = availableProducts; },
+        err => { }
+      )
+  }
+  initializeAuctions() {
+    this._marketplaceService.getAuctions()
+      .subscribe(
+        auctions => { this.auctions = auctions; },
+        err => { }
+      )
+  }
+
+
 
 }
